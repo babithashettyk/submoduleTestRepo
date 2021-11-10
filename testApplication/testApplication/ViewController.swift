f633812fcf1d086f84826530219cfc311e3d2960
Babitha Shetty K <babitha.shetty@globaldelight.com>
2021-11-10 14:35:37 +0530

    code commit

---
+++ testApplication/testApplication/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testApplication
+//
+//  Created by Babitha Shetty K on 10/11/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
